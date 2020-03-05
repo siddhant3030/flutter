bf12ca4ab03ccfd00868de8d782b22da8e910cc9
siddhant3030 <ssiddhant3030@gmail.com>
2020-03-05 19:41:45 +0530

    scratch

--- src/clojuretutorial/core.clj
+++
@@ -1,7 +0,0 @@
-(ns clojuretutorial.core
-  (:gen-class))
-
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
--- test/clojuretutorial/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns clojuretutorial.core-test
-  (:require [clojure.test :refer :all]
-            [clojuretutorial.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
