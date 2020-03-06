c5ae254382a1d9444a1778abe4f1226e88f45bc2
siddhant3030 <ssiddhant3030@gmail.com>
2020-03-06 14:22:07 +0530

    stateful widget

--- first_app/lib/home.dart
+++ first_app/lib/home.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+// class HomePage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       color: Colors.pink,
+//       child: Text('hey', style: TextStyle(color: Colors.white),)
+//     );
+//   }
+// }
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.pink,
-      child: Text('hey', style: TextStyle(color: Colors.white),)
+      child: Text('Im state'),
     );
   }
 }
