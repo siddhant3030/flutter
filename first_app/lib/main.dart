9c134969036f04b95c18ac7b58fef6da3227ff8d
siddhant3030 <ssiddhant3030@gmail.com>
2020-03-06 15:51:23 +0530

    shopping button added and search button added

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
-import 'home.dart';
+
 
 void main() {
   runApp(
     new MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomePage()
     )
   );
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        backgroundColor: Colors.amber,
+        title: Text('NoFilter'),
+        actions: <Widget>[
+          new IconButton(icon: Icon(Icons.search, color: Colors.white), onPressed: (){},),
+          new IconButton(icon: Icon(Icons.shopping_cart, color: Colors.white), onPressed: (){},)
+        ],
+      ),
+    );
+  }
 }
