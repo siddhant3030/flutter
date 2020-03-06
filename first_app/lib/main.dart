ff26a4d28902f1c0a30c053bbc6eaf8ad80da5c5
siddhant3030 <ssiddhant3030@gmail.com>
2020-03-06 12:30:15 +0530

    more child added

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -9,9 +9,19 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What is your fav colour?',
+      'What is your fav animal?'
+    ];
     return MaterialApp(home: Scaffold(
             appBar: AppBar(title: Text('My first App')),
-            body: Text('this is default'))
+            body: Column(children: [
+              Text('The question'),
+              RaisedButton(child: Text('Answer1'), onPressed: null),
+              RaisedButton(child: Text('Answer2'), onPressed: null),
+              RaisedButton(child: Text('Answer3'), onPressed: null),
+            
+            ],))
     );
    }
 }
