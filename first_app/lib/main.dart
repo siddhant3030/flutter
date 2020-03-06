89859462c3a4ec7d58fd681edf2e1e732bfeea53
siddhant3030 <ssiddhant3030@gmail.com>
2020-03-06 13:52:02 +0530

    flutter coloumn and row widget

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,27 +1,17 @@
 import 'package:flutter/material.dart';
 
-// void main() {
-//   runApp(MyApp());
-// }
+void main() {
+  runApp(
+    new MaterialApp(
+      home: new Center(
+        child: Row(children: <Widget>[
+          Text('Hello'),
+          Text('Hello2'),
+          Text('Hello3'),
+          Text('Hello4'),
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    var questions = [
-      'What is your fav colour?',
-      'What is your fav animal?'
-    ];
-    return MaterialApp(home: Scaffold(
-            appBar: AppBar(title: Text('My first App')),
-            body: Column(children: [
-              Text('The question'),
-              RaisedButton(child: Text('Answer1'), onPressed: null),
-              RaisedButton(child: Text('Answer2'), onPressed: null),
-              RaisedButton(child: Text('Answer3'), onPressed: null),
-            
-            ],))
-    );
-   }
+        ],)
+      ),
+    )
+  );
 }
