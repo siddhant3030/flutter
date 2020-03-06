5e57a94cc2e672594ff8741c643a3ad4d1253592
siddhant3030 <ssiddhant3030@gmail.com>
2020-03-06 14:17:21 +0530

    stateless widget

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -3,37 +3,17 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     new MaterialApp(
-      home: new Center(
-        child: ListView(children: <Widget>[
-          Text('Hello'),
-          Text('Hello2'),
-          Text('Hello3'),
-          Text('Hello4'),
-          Text('Hello4'),
-          Text('Hello4'),
-          Text('Hello4'),
-          Text('Hello4'),
-          Text('Hello4'),
-          Text('Hello4'),
-          Text('Hello4'),
-          Text('Hello4'),
-          Text('Hello4'),
-          Text('Hello4'),
-          Text('Hello4'),
-          Text('Hello4'),
-          Text('Hello4'),
-          Text('Hello4'),
-          Text('Hello4'),
-          Text('Hello4'),
-          Text('Hello4'),
-          Text('Hello4'),
-          Text('Hello4'),
-          Text('Hello4'),
-          Text('Hello4'),
-          Text('Hello4'),
-
-        ],)
-      ),
+      home: HomePage()
     )
   );
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.pink,
+      child: Text('hey', style: TextStyle(color: Colors.white),)
+    );
+  }
 }
